c1d17dd475c92d9e101e866fb35ce0a85f750a19
jasonka <jasonkatzman1@gmail.com>
2018-11-27 17:43:50 -0500

    Created get_project_list() to retrieve array of projects and render it to project_list.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,2 +1,15 @@
 <?php
 //application functions
+
+function get_project_list() {
+    include 'connection.php';
+
+    try {
+        return $db->query('SELECT project_id, title, category FROM projects');
+    } catch (PDOException $e) {
+        echo "Error!: " . $e->getMessage() . "<BR>";
+        return false;
+    }    
+}
+
+// var_dump(get_project_list());
